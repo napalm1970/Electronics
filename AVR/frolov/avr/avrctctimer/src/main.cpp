2b8b3b86d704ce2f4487a338cffe201a0e9d518e
napalm1970 <napalm1970@gmail.com>
2025-01-28 22:54:59 +0200

    ctc

---
+++ AVR/frolov/avr/avrctctimer/src/main.cpp
@@ -0,0 +1,20 @@
+// CTC - Clear Timer on Compare match
+
+#include <avr/io.h>
+
+void timer_ctc(void) {
+  TCCR0 |= (1 << WGM01); // CTC mode.
+  TCCR0 |= (1 << CS02);  //| (1 << CS20); // Prescaler 64
+  TCCR0 |= (1 << COM00); // Toggle compaare match.
+
+  DDRB |= (1 << PB4); // Pin OC2 output.
+  OCR0 = 50;          // F = (F_CPU/Prescaler)/(OCR2+1)*2
+}
+
+int main() {
+  timer_ctc();
+
+  while (1) {
+  }
+  return 0;
+}
